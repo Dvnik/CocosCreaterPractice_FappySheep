c5b79920f3eda36f394dfc35c6544004f4f1f922
dvnik <dvnik@local.low>
2017-05-24 11:54:34 +0800

    Sheep.js檔案完成

--- assets/scripts/Sheep.js
+++ assets/scripts/Sheep.js
@@ -30,15 +30,134 @@ var Sheep = cc.Class({
             type: State,
             visible: flase
         },
+        state: {
+            get: function () {
+                return this._state;
+            },
+            set: function(value){
+                if(value !== this._state) {
+                    this._state = value;
+                    if(this._state !== State.None) {
+                        var animName = State[this._state];
+                        this.anim.stop();
+                        this.anim.play(animName);
+                    }
+                }
+            },
+            type: State
+        },
+        //-- 获取Jump音效
+        jumpAudio: {
+            default: null,
+            url: cc.AudioClip
+        },
+        dustPrefab: cc.Prefab
+    },
+    statics: {
+        State: State
+    },
+    init () {
+        //-- 当前播放动画组件
+        this.anim = this.getComponent(cc.Animation);
+        //-- 当前速度
+        this.currentSpeed = 0;
+        //-- 绵羊图片渲染
+        this.sprite = this.getComponent(cc.Sprite);
+        this.registerInput();
+    },
+    startRun () {
+        this.state = State.Run;
+        this.enableInput(true);
+    },
+    //-- 初始化
+    registerInput () {
+        //-- 添加绵羊控制事件(为了注销事件缓存事件)
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: function(keyCode, event){
+                this.jump();
+            }.bind(this)
+        }, this.node);
+        // touch input
+        cc.eventManager.addListener({
+            event: cc.EventListener.TOUCH_ONE_BY_ONE,
+            onTouchBegan: function(touch, evnet){
+                this.jump();
+                return true;
+            }.bind(this)
+        }, this.node);
+    },
+    //-- 删除
+    enableInput: function (enable) {
+        if (enable) {
+            cc.eventManager.resumeTarget(this.node);
+        } else {
+            cc.eventManager.pauseTarget(this.node);
+        }
     },
 
-    // use this for initialization
-    onLoad: function () {
-
+    //-- 更新
+    update (dt) {
+        switch(this.state){
+            case State.Jump:
+                if (this.currentSpeed < 0) {
+                    this.state = State.Drop;
+                }
+                break;
+            case State.Drop:
+                if (this.node.y < this.groundY) {
+                    this.node.y = this.groundY;
+                    this.state = State.DropEnd;
+                    this.spawnDust('DustDown');
+                }
+            case State.None:
+            case State.Dead:
+                return;
+        }
+        var flying = this.state === State.Jump || this.node.y > this.groundY;
+        if(flying) {
+            this.currentSpeed -= dt * this.gravity;
+            this.node.y += dt * this.currentSpeed;
+        }
     },
 
-    // called every frame, uncomment this function to activate update callback
-    // update: function (dt) {
+    // invoked by animation
+    onDropFinished () {
+        this.state = State.Run;
+    },
 
-    // },
+    onCollisionEnter: function (other) {
+        if (this.state !== State.Dead){
+            var group = cc.game.groupList[other.node.groupIndex];
+            if(group === 'Obstacle') {
+                // bump
+                this.state = Sheep.State.Dead;
+                D.game.gameOver();
+                this.enableInput(false);
+            }
+            else if (group === 'NextPipe') {
+                // jump over
+                D.game.gainScore();
+            }
+        }
+    },
+
+    jump: function () {
+        this.state = State.Jump;
+        this.currentSpeed = this.initJumpSpeed;
+        //-- 播放跳音效
+        cc.audioEngine.playEffect(this.jumpAudio);
+        this.spawnDust('DustUp');
+    },
+    spawnDust (animName) {
+        let dust = null;
+        if (cc.pool.hasObject(Dust)) {
+            dust = cc.pool.getFromPool(Dust);
+        } else {
+            dust = cc.instantiate(this.dustPrefab).getComponent(Dust);
+        }
+        this.node.parent.addChlid(dust.node);
+        dust.node.position = this.node.position;
+        dust.playAnim(animName);
+    }
 });
